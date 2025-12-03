cf7c1ef9ec380cea4ed6e9a4e8ae2e80db8081f7
Ivar Fatland <fatland99@hotmail.com>
2025-12-03 18:29:03 +0100

    remove resolved todo

--- cig.h
+++ cig.h
@@ -76,7 +76,6 @@ allocator_t allocator_from_borrow(borrow_allocator_t *this);
 // with_borrow(foos_allocator) {bar = foo(foos_allocator, my_allocator); } Using
 // the return a keyword in the statement following this macro will cause a
 // guaranteed memory leak.
-// TODO: simplify!
 #define with_borrow(NAME)                                                      \
   for (allocator_t NAME = borrow_allocator_create(); NAME.this != NULL;        \
        NAME.this = (allocator_reset(NAME), NULL))                              \
